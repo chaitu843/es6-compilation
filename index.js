b6151ff7034e6c0661c41ea82f2ae89e2414e842
credd3 <creddy4@sapient.com>
2019-11-11 14:50:44 +0530

    'UpToDate'

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-import uuid from 'uuid';
-console.log(uuid.v4());
+var fetch = require("node-fetch");
+
+const apiKey = '239a46eb92e555f8c2bca24a2637555a';
+let searchQuery = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`,
+    movieDetailsByIdQuery = ``;
+
+let getMovieRatingByName = (name) => {
+    fetch(`${searchQuery}${name}`)
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp.results[0].vote_average)
+        // fetch(`${movieDetailsByIdQuery}/${resp.results[0].id}`)
+        // .then(resp => resp.json())
+        // .then(resp => console.log(resp))
+    })
+}
